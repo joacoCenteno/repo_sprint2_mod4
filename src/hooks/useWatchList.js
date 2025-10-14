58d6f3f70ccb4b68e63bd1540d56eb0f23dc48c9
joacoCenteno <centenojoaco01@gmail.com>
2025-10-14 10:50:22 -0300

    Implementation cutom hook

---
+++ src/hooks/useWatchList.js
@@ -0,0 +1,28 @@
+import { useState, useEffect } from "react";
+
+const useWacthList = () =>{
+    const [watchlist, setWatchList] = useState([]);
+
+    const addToWatchList = (game) =>{
+      setWatchList([...watchlist,game]);
+      localStorage.setItem("watchlist", JSON.stringify([...watchlist,game]))
+    }
+    
+    const removeFromWatchList = (id) =>{
+      const updatedList = watchlist.filter((game => game.id != id));
+      setWatchList(updatedList);
+      localStorage.setItem("watchlist", JSON.stringify(updatedList))
+    }
+
+  
+    useEffect(() => {
+        const savedWatchList = JSON.parse(localStorage.getItem("watchlist")) || [];
+        setWatchList(savedWatchList);
+    }, []);
+
+
+    return {watchlist, addToWatchList, removeFromWatchList, useEffect}
+}
+
+
+export default useWacthList;
